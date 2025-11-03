5a5ea3640a3c9bf06e2c512b3003963343619f0a
mathieu fontaine <mathieufontaine128@gmail.com>
2025-11-03 13:21:05 +0100

    lundi 03-11-split

--- libft.h
+++ libft.h
@@ -33,3 +33,4 @@ void	*ft_calloc(size_t memb, size_t size);i
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char  const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(const char *s, char c);
